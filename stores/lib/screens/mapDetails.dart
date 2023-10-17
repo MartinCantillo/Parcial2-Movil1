74110507bcde2a31db32d02c9556cfc65a62823e
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2023-10-17 13:31:23 -0500

    card2

--- stores/lib/screens/mapDetails.dart
+++ stores/lib/screens/mapDetails.dart
@@ -1,30 +1,29 @@
+
 import 'package:flutter/material.dart';
 
 class MapItemDetails extends StatefulWidget {
-  final String tiendaNombre;
-  const MapItemDetails({
-    super.key,
-    required this.tiendaNombre,
-  });
+final String tiendaNombre;
+  const MapItemDetails({super.key,  required this.tiendaNombre,} );
 
   @override
   State<MapItemDetails> createState() => _MapItemDetailsState();
 }
 
 class _MapItemDetailsState extends State<MapItemDetails> {
-  String get tiendaNombre => widget.tiendaNombre;
+  String get tiendaNombre => tiendaNombre;
 
+ 
   @override
   Widget build(BuildContext context) {
     return Padding(padding: const EdgeInsets.all(15.0),
-     child: Card(
+    child: Card(
+      
       color: Colors.white,
       child: Row(
-        children: [
-          Expanded(child:  Image.asset("assets/images/tienda.png",  ))
-        ],
+        children: [Expanded(child: Image.asset("assets/images/marker2.png" ), ), Expanded(child: Column(children: [Text(tiendaNombre), Text("hola2")],))],
       ),
-     ),
+    ),
+    
     );
   }
 }
